aed7c516a7445aca58caa34b8b100c4766b66ef9
AJ197028 <aksjais20@gmail.com>
2023-05-05 16:05:59 +0530

    Subscribing and Dispatching Actions

---
+++ src/components/Counter.js
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'IncrementBy5' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DecrementBy5' });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>IncrementBy5</button>
+        <button onClick={decrementHandler}>DecrementBy5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = {counter: 0}, action) => {
+    if(action.type === 'IncrementBy5') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+    if(action.type === 'DecrementBy5') {
+        return {
+            counter: state.counter - 5
+        }
+    }
+    return state;
+}
+
+const store = createStore(counterReducer);
+
+export default store;
